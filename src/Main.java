98a19c1f35de4a435a8da8053ab118725749256d
Bobby Grow <smileatyourself8@gmail.com>
2022-10-11 18:24:18 +0300

    added params check logic with exceptions

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@
 
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
@@ -20,21 +21,26 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-
+        checkArgs(args);
         OperationMode mode = OperationMode.valueOf(args[0].toUpperCase());
         String srcFile = args[1];
-        int key = Integer.parseInt(args[2]);
-
 
         switch (mode) {
-            case ENCODE -> encodeOrDecode(mode, srcFile, key);
-            case DECODE -> encodeOrDecode(mode, srcFile, -key);
-            case BRUTEFORCE -> bruteforce();
+            case ENCODE -> {
+                int key = Integer.parseInt(args[2]);
+                encodeOrDecodeFile(mode, srcFile, key);
+            }
+            case DECODE -> {
+                int key = Integer.parseInt(args[2]);
+                encodeOrDecodeFile(mode, srcFile, key * -1);
+            }
+            case BRUTEFORCE -> {
+                bruteforce();
+            }
         }
     }
 
-    private static void encodeOrDecode(OperationMode mode, String srcFile, int key) {
+    private static void encodeOrDecodeFile(OperationMode mode, String srcFile, int key) {
         String destFile = resolveDestFilename(srcFile, mode);
         try (FileReader reader = new FileReader(srcFile);
              StringWriter stringWriter = new StringWriter();
@@ -45,7 +51,7 @@ public class Main {
             while (reader.ready()) {
                 int real = reader.read(buffer);
                 stringWriter.write(buffer, 0, real);
-                encodedString = encodeString(stringWriter.toString(), key);
+                encodedString = encodeOrDecodeString(stringWriter.toString(), key);
             }
             fileWriter.write(encodedString);
 
@@ -55,7 +61,7 @@ public class Main {
 
     }
 
-    private static String encodeString(String text, int key) {
+    private static String encodeOrDecodeString(String text, int key) {
         char[] alphabetArr = ALPHABETS.toCharArray();
         char[] textArr = text.toCharArray();
         StringBuilder stringBuilder = new StringBuilder();
@@ -95,7 +101,14 @@ public class Main {
 
         if (mode == OperationMode.ENCODE) {
             filenameStr = filenameStr + "(encoded)" + fileExtension;
+
         } else if (mode == OperationMode.DECODE) {
+            int bracketLocation = pathString.lastIndexOf('(');
+            if (bracketLocation == -1) {
+                bracketLocation = pointLocation;
+            }
+            filenameStr = pathString.substring(0, bracketLocation);
+            fileExtension = pathString.substring(pointLocation);
             filenameStr = filenameStr + "(decoded)" + fileExtension;
         }
 
@@ -106,5 +119,35 @@ public class Main {
         //TODO: bruteforce
     }
 
+    private static void checkArgs(String[] args) throws IllegalArgumentException {
+        if (args[0] == null || args[1] == null || args[2] == null) {
+            throw new IllegalArgumentException("Неверное количество аргументов командной строки. Используй operation filePath key.");
+        } else if (Files.notExists(Path.of(args[1]))) {
+            throw new IllegalArgumentException("Исходный файл не найден.");
+        } else if (!args[0].equalsIgnoreCase("encode") &&
+                !args[0].equalsIgnoreCase("decode") &&
+                !args[0].equalsIgnoreCase("bruteforce")) {
+            throw new IllegalArgumentException("Неверный первый аргумент. Ожидаю один из: encode|decode|bruteforce.");
+        } else if (args[0].equalsIgnoreCase("bruteforce")) {
+            if (Files.notExists(Path.of(args[2]))) {
+                throw new IllegalArgumentException("Не могу найти файл для статистического анализа кода. Ожидаю аргументы: bruteforce filepath filepath2.");
+            }
+        } else if (!isInteger(args[2])) {
+            throw new IllegalArgumentException("Неверный ключ кодирования в третьем параметре. Ожидаю целое ненулевое число.");
+        } else if (Integer.parseInt(args[2]) == 0) {
+            throw new IllegalArgumentException(("Нет смысла кодировать ключом 0. Ожидаю целое ненулевое число в параметре 3."));
+        }
+    }
 
+    private static boolean isInteger(String str) {
+        if (str == null) {
+            return false;
+        }
+        try {
+            int a = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
 }
