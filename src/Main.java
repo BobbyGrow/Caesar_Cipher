ed97e52f241846a2949b48f2e76a614c7f841d54
Bobby Grow <smileatyourself8@gmail.com>
2022-10-12 12:22:31 +0300

    ready for bruteforce

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
-    enum OperationMode {
+    enum Mode {
         ENCODE,
         DECODE,
         BRUTEFORCE
@@ -20,48 +20,52 @@ public class Main {
     static final int ABC2_LENGTH = ALPHABETS.length() - ABC1_LENGTH;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         checkArgs(args);
-        OperationMode mode = OperationMode.valueOf(args[0].toUpperCase());
+        Mode mode = Mode.valueOf(args[0].toUpperCase());
         String srcFile = args[1];
 
         switch (mode) {
             case ENCODE -> {
                 int key = Integer.parseInt(args[2]);
-                encodeOrDecodeFile(mode, srcFile, key);
+                String encodedText = encodeFileToString(mode, srcFile, key);
+                String destFile = resolveDestFilename(srcFile, mode);
+                writeCodeToFile(encodedText, destFile);
             }
             case DECODE -> {
-                int key = Integer.parseInt(args[2]);
-                encodeOrDecodeFile(mode, srcFile, key * -1);
+                int key = Integer.parseInt(args[2]) * -1;
+                String encodedText = encodeFileToString(mode, srcFile, key);
+                String destFile = resolveDestFilename(srcFile, mode);
+                writeCodeToFile(encodedText, destFile);
             }
             case BRUTEFORCE -> {
-                bruteforce();
+                bruteforce(srcFile, args[2]);
             }
         }
     }
 
-    private static void encodeOrDecodeFile(OperationMode mode, String srcFile, int key) {
-        String destFile = resolveDestFilename(srcFile, mode);
+    private static String encodeFileToString(Mode mode, String srcFile, int key) {
+
         try (FileReader reader = new FileReader(srcFile);
-             StringWriter stringWriter = new StringWriter();
-             FileWriter fileWriter = new FileWriter(destFile)) {
+             StringWriter stringWriter = new StringWriter()) {
 
             char[] buffer = new char[4096];
-            String encodedString = "";
+            String encodedText = "";
             while (reader.ready()) {
                 int real = reader.read(buffer);
                 stringWriter.write(buffer, 0, real);
-                encodedString = encodeOrDecodeString(stringWriter.toString(), key);
+                encodedText = encodePartOfFile(stringWriter.toString(), key);
             }
-            fileWriter.write(encodedString);
+            return encodedText;
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        return null;
     }
 
-    private static String encodeOrDecodeString(String text, int key) {
+    private static String encodePartOfFile(String text, int key) {
         char[] alphabetArr = ALPHABETS.toCharArray();
         char[] textArr = text.toCharArray();
         StringBuilder stringBuilder = new StringBuilder();
@@ -71,16 +75,18 @@ public class Main {
             if (Character.isLetter(temp)) {
                 int i = ALPHABETS.indexOf(temp);
                 if (i >= 0 && i <= ABC1_LENGTH - 1) {
-                    if (i + key < 0) {
-                        temp = alphabetArr[(i + key + ABC1_LENGTH)];
+                    int nKey = normalizeKey(key, ABC1_LENGTH);
+                    if (i + nKey < 0) {
+                        temp = alphabetArr[(i + nKey + ABC1_LENGTH)];
                     } else {
-                        temp = alphabetArr[(i + key) % ABC1_LENGTH];
+                        temp = alphabetArr[(i + nKey) % ABC1_LENGTH];
                     }
                 } else if (i >= ABC1_LENGTH) {
-                    if (i + key < ABC1_LENGTH) {
-                        temp = alphabetArr[i + key + ABC2_LENGTH];
+                    int nKey = normalizeKey(key, ABC2_LENGTH);
+                    if (i + nKey < ABC1_LENGTH) {
+                        temp = alphabetArr[i + nKey + ABC2_LENGTH];
                     } else {
-                        temp = alphabetArr[((i - ABC1_LENGTH + key) % ABC2_LENGTH) + ABC1_LENGTH];
+                        temp = alphabetArr[((i - ABC1_LENGTH + nKey) % ABC2_LENGTH) + ABC1_LENGTH];
                     }
                 }
                 if (Character.isUpperCase(symbol)) {
@@ -93,16 +99,31 @@ public class Main {
         return stringBuilder.toString();
     }
 
-    private static String resolveDestFilename(String srcFile, OperationMode mode) {
+    private static void writeCodeToFile(String encodedText, String destFile) {
+        try (FileWriter fileWriter = new FileWriter(destFile)) {
+
+            fileWriter.write(encodedText);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int normalizeKey(int key, int length) {
+        key = key % length;
+        return key;
+    }
+
+    private static String resolveDestFilename(String srcFile, Mode mode) {
         String pathString = Path.of(srcFile).getFileName().toString();
         int pointLocation = pathString.lastIndexOf('.');
         String filenameStr = pathString.substring(0, pointLocation);
         String fileExtension = pathString.substring(pointLocation);
 
-        if (mode == OperationMode.ENCODE) {
+        if (mode == Mode.ENCODE) {
             filenameStr = filenameStr + "(encoded)" + fileExtension;
 
-        } else if (mode == OperationMode.DECODE) {
+        } else if (mode == Mode.DECODE) {
             int bracketLocation = pathString.lastIndexOf('(');
             if (bracketLocation == -1) {
                 bracketLocation = pointLocation;
@@ -115,11 +136,12 @@ public class Main {
         return filenameStr;
     }
 
-    private static void bruteforce() {
-        //TODO: bruteforce
+    private static void bruteforce(String srcFile, String arg){
+
     }
 
-    private static void checkArgs(String[] args) throws IllegalArgumentException {
+
+    private static void checkArgs(String[] args) throws IllegalArgumentException, IOException {
         if (args[0] == null || args[1] == null || args[2] == null) {
             throw new IllegalArgumentException("Неверное количество аргументов командной строки. Используй operation filePath key.");
         } else if (Files.notExists(Path.of(args[1]))) {
@@ -136,6 +158,8 @@ public class Main {
             throw new IllegalArgumentException("Неверный ключ кодирования в третьем параметре. Ожидаю целое ненулевое число.");
         } else if (Integer.parseInt(args[2]) == 0) {
             throw new IllegalArgumentException(("Нет смысла кодировать ключом 0. Ожидаю целое ненулевое число в параметре 3."));
+        } else if (Files.size(Path.of(args[1])) == 0) {
+            throw new IllegalArgumentException("Исходный файл пуст. Кодировать нечего.");
         }
     }
 
